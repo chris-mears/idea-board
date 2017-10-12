b7431971efd00d212032a0fd8147f1718472a333
SadEvilPenguin <mears.chris@gmail.com>
2017-10-12 11:08:06 -0400

    updated

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
+
 mongoose.Promise = global.Promise
 mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true})
+
 
 const connection = mongoose.connection
 connection.on('connected', () => {
